df916a9da318a1a3a38443bb0b316adcd42714c4
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2023-07-06 15:35:51 +0300

    Insert tables

---
+++ sql/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO cats (name, date_birth, commands)
+VALUES
+    ('Myrzik','01.10.2022','sit, lie down, speak'),
+    ('Bella', '03.12.2022', 'sit, lie down, speak, jump'),
+    ('Vasya', '10.10.2021', 'sit, lie down, speak'),
+    ('Max', '05.02.2023', 'sit, lie down, speak, jump');
+
+INSERT INTO dogs (name, date_birth, commands)
+VALUES
+    ('Alex','01.08.2020','sit, lie down, speak, jump, paw'),
+    ('Puma', '11.05.2021', 'sit, lie down, speak, jump'),
+    ('Rex', '23.09.2022', 'sit, lie down, speak, paw'),
+    ('Mila', '19.01.2023', 'sit, lie down, speak, jump');
+
+INSERT INTO hamsters (name, date_birth, commands)
+VALUES
+    ('Homa','08.12.2022','sit, lie down, run, jump'),
+    ('Umka', '21.02.2023', 'sit, lie down, jump'),
+    ('Gold', '05.10.2022', 'sit, lie down, run, jump');
+
+INSERT INTO horses (name, date_birth, commands)
+VALUES
+    ('Black','03.03.2020','sit, lie down, run, jump, speak'),
+    ('Sky', '10.01.2021', 'sit, lie down, jump, run');
+
+INSERT INTO camels (name, date_birth, commands)
+VALUES
+    ('Ali','15.05.2022','sit, lie down, run'),
+    ('Abu','02.01.2023','sit, lie down, run');
+
+INSERT INTO horses (name, date_birth, commands)
+VALUES
+    ('Gray','11.07.2021','sit, lie down, run, speak');
