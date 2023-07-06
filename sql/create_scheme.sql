d2ff15489ed7ab009fbe82d5e621e7cb628ec91a
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2023-07-06 16:56:20 +0300

    Create and insert tables

--- sql/create_scheme.sql
+++ sql/create_scheme.sql
@@ -3,7 +3,7 @@ create table cats
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   VARCHAR(255) null,
     constraint cats_pk
         primary key (id)
@@ -13,7 +13,7 @@ create table dogs
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   varchar(255) null,
     constraint dogs_pk
         primary key (id)
@@ -23,7 +23,7 @@ create table hamsters
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   varchar(255) null,
     constraint hamsters_pk
         primary key (id)
@@ -33,7 +33,7 @@ create table horses
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   varchar(255) null,
     constraint horses_pk
         primary key (id)
@@ -43,7 +43,7 @@ create table camels
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   varchar(255) null,
     constraint camels_pk
         primary key (id)
@@ -52,7 +52,7 @@ create table donkeys
 (
     id         bigint auto_increment,
     name       varchar(255) not null,
-    date_birth timestamp    not null,
+    date_birth date    not null,
     commands   varchar(255) null,
     constraint donkeys_pk
         primary key (id)
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,33 +1,33 @@
 INSERT INTO cats (name, date_birth, commands)
 VALUES
-    ('Myrzik','01.10.2022','sit, lie down, speak'),
-    ('Bella', '03.12.2022', 'sit, lie down, speak, jump'),
-    ('Vasya', '10.10.2021', 'sit, lie down, speak'),
-    ('Max', '05.02.2023', 'sit, lie down, speak, jump');
+    ('Myrzik','2022-11-01','sit, lie down, speak'),
+    ('Bella', '2022-12-03', 'sit, lie down, speak, jump'),
+    ('Vasya', '2021-10-10', 'sit, lie down, speak'),
+    ('Max', '2023-02-05', 'sit, lie down, speak, jump');
 
 INSERT INTO dogs (name, date_birth, commands)
 VALUES
-    ('Alex','01.08.2020','sit, lie down, speak, jump, paw'),
-    ('Puma', '11.05.2021', 'sit, lie down, speak, jump'),
-    ('Rex', '23.09.2022', 'sit, lie down, speak, paw'),
-    ('Mila', '19.01.2023', 'sit, lie down, speak, jump');
+    ('Alex','2020-08-01','sit, lie down, speak, jump, paw'),
+    ('Puma', '2021-05-11', 'sit, lie down, speak, jump'),
+    ('Rex', '2022-09-23', 'sit, lie down, speak, paw'),
+    ('Mila', '2023-01-19', 'sit, lie down, speak, jump');
 
 INSERT INTO hamsters (name, date_birth, commands)
 VALUES
-    ('Homa','08.12.2022','sit, lie down, run, jump'),
-    ('Umka', '21.02.2023', 'sit, lie down, jump'),
-    ('Gold', '05.10.2022', 'sit, lie down, run, jump');
+    ('Homa','2022-12-08','sit, lie down, run, jump'),
+    ('Umka', '2023-02-21', 'sit, lie down, jump'),
+    ('Gold', '2022-10-05', 'sit, lie down, run, jump');
 
 INSERT INTO horses (name, date_birth, commands)
 VALUES
-    ('Black','03.03.2020','sit, lie down, run, jump, speak'),
-    ('Sky', '10.01.2021', 'sit, lie down, jump, run');
+    ('Black','2020-03-03','sit, lie down, run, jump, speak'),
+    ('Sky', '2021-01-10', 'sit, lie down, jump, run');
 
 INSERT INTO camels (name, date_birth, commands)
 VALUES
-    ('Ali','15.05.2022','sit, lie down, run'),
-    ('Abu','02.01.2023','sit, lie down, run');
+    ('Ali','2022-05-15','sit, lie down, run'),
+    ('Abu','2023-01-02','sit, lie down, run');
 
 INSERT INTO donkeys (name, date_birth, commands)
 VALUES
-    ('Gray','11.07.2021','sit, lie down, run, speak');
+    ('Gray','2021-07-01','sit, lie down, run, speak');
